3a74b9ce80d9e7640e375b46940ad27b460ae55a
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2022-09-02 19:59:39 -0700

    header part complete

---
+++ js/categories.js
@@ -0,0 +1,41 @@
+const categoriesDisplay = () => {
+    fetch("https://openapi.programming-hero.com/api/news/categories")
+        .then(res => res.json())
+        .then(data => displayCategory(data.data.news_category));
+}
+
+
+const displayCategory = categories => {
+    const parent = document.getElementById('categories');
+
+
+
+    categories.forEach(category => {
+        const child = document.createElement('a');
+        child.classList.add('category');
+        const categoryName = category.category_name;
+        child.className += " " + categoryName;
+        child.innerText = category.category_name;
+        parent.appendChild(child);
+    });
+
+    const entertainment = parent.getElementsByClassName('Entertainment');
+    entertainment[0].classList.add('active');
+
+
+    ///active category
+
+    var btns = parent.getElementsByClassName("category");
+
+
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace("active", "");
+            this.className += " active";
+        });
+    }
+}
+
+
+categoriesDisplay();
